9bb0f72e9c9e2076cb9c94f348357bf8fd01b260
javierrochadev <jrocha@stemdo.io>
2024-12-05 12:01:41 +0100

    se añaden permisos

--- cert_creation.sh
+++ cert_creation.sh
@@ -18,6 +18,10 @@ CERT_FILE="tls.crt"
 echo "Generando clave privada..."
 openssl genpkey -algorithm RSA -out $KEY_FILE -aes256 -pass pass:$PASSPHRASE
 
+# Establecer permisos de lectura sobre el archivo de clave privada
+chmod 644 $KEY_FILE
+echo "Permisos de lectura establecidos para $KEY_FILE"
+
 # 2. Generar la solicitud de certificado (CSR)
 echo "Generando solicitud de certificado (CSR)..."
 openssl req -new -key $KEY_FILE -out tls.csr -passin pass:$PASSPHRASE -subj "/C=US/ST=State/L=City/O=Company/OU=IT/CN=localhost"
@@ -26,9 +30,12 @@ openssl req -new -key $KEY_FILE -out tls.csr -passin pass:$PASSPHRASE -subj "/C=
 echo "Generando certificado autofirmado..."
 openssl x509 -req -in tls.csr -out $CERT_FILE -signkey $KEY_FILE -days 365 -passin pass:$PASSPHRASE
 
+# Establecer permisos de lectura sobre el archivo de certificado
+chmod 644 $CERT_FILE
+echo "Permisos de lectura establecidos para $CERT_FILE"
+
 # Limpiar el archivo CSR (no es necesario después de la firma)
 rm tls.csr
 
 echo "Certificado y clave generados correctamente."
-echo "Certificado: $CERT_FILE"
-echo "Clave privada: $KEY_FILE"
+
